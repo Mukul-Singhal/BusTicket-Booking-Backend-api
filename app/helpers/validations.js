e3772a96bdda144cab77868ad2c709c2641a06a4
mukul <mukulsinghal98@gmail.com>
2021-03-31 23:58:56 +0530

    Added jwt and generate jwt token

--- app/helpers/validations.js
+++ app/helpers/validations.js
@@ -1,11 +1,31 @@
+/* eslint-disable camelcase */
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import env from "../../env";
+/**
+ * Hash Password Method
+ * @param {string} password
+ * @returns {string} returns hashed password
+ */
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+const hashPassword = (password) => bcrypt.hashSync(password, salt);
+
+/**
+ * comparePassword
+ * @param {string} hashPassword
+ * @param {string} password
+ * @returns {Boolean} return True or False
+ */
+const comparePassword = (hashedPassword, password) => {
+  return bcrypt.compareSync(password, hashedPassword);
+};
 
 /**
  * isValidEmail helper method
  * @param {string} email
  * @returns {Boolean} True or False
  */
-
 const isValidEmail = (email) => {
   const regEx = /\S+@\S+\.\S+/;
   return regEx.test(email);
@@ -48,4 +68,32 @@ const empty = (input) => {
   }
 };
 
-export { isValidEmail, validatePassword, isEmpty, empty };
+/**
+ * Generate Token
+ * @param {string} id
+ * @returns {string} token
+ */
+const generateUserToken = (email, id, is_admin, first_name, last_name) => {
+  const token = jwt.sign(
+    {
+      email,
+      user_id: id,
+      is_admin,
+      first_name,
+      last_name,
+    },
+    env.secret,
+    { expiresIn: "3d" }
+  );
+  return token;
+};
+
+export {
+  hashPassword,
+  comparePassword,
+  isValidEmail,
+  validatePassword,
+  isEmpty,
+  empty,
+  generateUserToken,
+};
