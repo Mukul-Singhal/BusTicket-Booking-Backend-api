04922fefc7e449b967c5fc0d044f297fbb41b551
mukul <mukulsinghal98@gmail.com>
2021-04-01 00:00:23 +0530

    Added some comments

--- app/db/dev/pool.js
+++ app/db/dev/pool.js
@@ -2,10 +2,12 @@ import { Pool } from "pg";
 
 import dotenv from "dotenv";
 
+//To use environment variables
 dotenv.config();
 
 const databaseConfig = { connectionString: process.env.DATABASE_URL };
 
+//create a new pool
 const pool = new Pool(databaseConfig);
 
 export default pool;
